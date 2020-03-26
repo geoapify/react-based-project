e5e7dda233fa8e568f719b5c9993d80f9f5641ed
Alfiya Tarasenko <alfiya.tarasenko@geoapify.com>
2020-03-26 06:45:45 +0100

    First veraion

---
+++ src/components/my-map.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './my-map.scss';
+
+function MyMap() {
+  return (
+    <div className="map-container">
+      <div className="placeholder">
+        <span className="placeholder-text">The map will be displayed here</span>
+      </div>
+    </div>
+  )
+}
+
+export default MyMap;
